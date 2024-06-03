d3bfec33327cf744c096165431124d1ba1e1ee59
harukari007 <harukari007@gmail.com>
2024-06-04 03:36:58 +0900

    first commit

---
+++ recipe_create.php
@@ -0,0 +1,92 @@
+<?php
+// POSTデータ確認
+// var_dump($_POST);
+// exit();
+    // タイムゾーンを日本時間に設定
+    // date_default_timezone_set('Asia/Tokyo');
+    // print_r($_POST);
+    
+    echo htmlspecialchars($_POST['recipe_name'], ENT_QUOTES, 'UTF-8');
+    // デバック用の関数
+    echo "<br>";
+
+    // カテゴリー事の関数、条件分岐
+if ($_POST['category'] === '1') echo "和食";
+if ($_POST['category'] === '2') echo "中華";
+if ($_POST['category'] === '3') echo "洋食";
+echo "<br>";
+
+if ($_POST['difficulty'] === '1'){
+    echo "簡単";
+} elseif ($_POST['difficulty']==='2'){
+    echo "普通";
+} elseif($_POST['difficulty']==='3'){
+    echo "難しい";
+}
+echo "<br>";
+
+// 入力内容が数字であるかを確認する為の条件を指定する
+if (is_numeric($_POST['budget'])) {
+    echo number_format($_POST['budget']);
+}
+echo "<br>";
+
+// nl2br関数を活用して、改行コードを反映させる
+echo nl2br(htmlspecialchars($_POST['howto'],ENT_QUOTES,'UTF-8'));
+echo "<br>";
+
+$recipe_name = $_POST['recipe_name'];
+$category = $_POST['category'];
+$difficulty = $_POST['difficulty'];
+$budget = $_POST['budget'];
+$howto = $_POST['howto'];
+
+// 各種項目設定
+$dbn = 'mysql:dbname=gs_lab10_01;charset=utf8mb4;port=3306;host=localhost';
+$user = 'root';
+$pwd = '';
+
+// DB接続
+try {
+    $pdo = new PDO($dbn, $user, $pwd);
+} catch (PDOException $e) {
+    echo json_encode(["db error" => "{$e->getMessage()}"]);
+    exit();
+}
+
+// DB接続
+if (
+    !isset($_POST['recipe_name']) || $_POST['recipe_name'] === '' ||
+    !isset($_POST['category']) || $_POST['category'] === '' ||
+    !isset($_POST['difficulty']) || $_POST['difficulty'] === '' ||
+    !isset($_POST['budget']) || $_POST['budget'] === '' ||
+    !isset($_POST['howto']) || $_POST['howto'] === ''
+) {
+    exit('接続されていません');
+}
+
+
+// SQL作成&実行
+
+$sql = 'INSERT INTO recipes (id, recipe_name, category, difficulty, budget,howto) VALUES (NULL, :recipe_name, :category, :difficulty, :budget,:howto)';
+$stmt = $pdo->prepare($sql);
+
+// バインド変数を設定 仮置きする、
+$stmt->bindValue(':recipe_name', $recipe_name, PDO::PARAM_STR);
+$stmt->bindValue(':category', $category, PDO::PARAM_STR);
+$stmt->bindValue(':difficulty', $difficulty, PDO::PARAM_STR);
+$stmt->bindValue(':budget', $budget, PDO::PARAM_STR);
+$stmt->bindValue(':howto', $howto, PDO::PARAM_STR);
+
+// SQL実行（実行に失敗すると `sql error ...` が出力される）
+try {
+    $status = $stmt->execute();
+    // 動かしている場所
+} catch (PDOException $e) {
+    echo json_encode(["sql error" => "{$e->getMessage()}"]);
+    exit();
+    // SQL文のエラーが一番多い
+}
+
+header('Location:form.html');
+exit();
---
+++ recipe_read.php
@@ -0,0 +1,87 @@
+<?php
+
+// DB接続
+$dbn = 'mysql:dbname=gs_lab10_01;charset=utf8mb4;port=3306;host=localhost';
+$user = 'root';
+$pwd = '';
+
+try {
+    $pdo = new PDO($dbn, $user, $pwd);
+} catch (PDOException $e) {
+    echo json_encode(["db error" => "{$e->getMessage()}"]);
+    exit();
+}
+
+
+// SQL作成&実行
+$sql = 'SELECT * FROM recipes;';
+$stmt = $pdo->prepare($sql);
+
+try {
+    $status = $stmt->execute();
+} catch (PDOException $e) {
+    echo json_encode(["sql error" => "{$e->getMessage()}"]);
+    exit();
+}
+
+// SQL実行の処理
+
+$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+// データが配列で格納されているか確認する為のコマンド
+// echo "<pre>";
+// var_dump($result);
+// echo"</pre>";
+// exit();
+
+$output = "";
+foreach ($result as $record) {
+    $output .= "
+    <tr>
+    <td>{$record["recipe_name"]}</td>
+    <td>{$record["category"]}</td>
+    <td>{$record["difficulty"]}</td>
+    <td>{$record["budget"]}</td>
+    <td>{$record["howto"]}</td>
+    </tr>";
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>設備導入手順（一覧画面）</title>
+</head>
+
+<body>
+    <fieldset>
+        <legend>設備導入手順（一覧画面）</legend>
+        <a href="form.html">入力画面</a>
+        <table>
+            <thead>
+                <tr>
+                    <th>設備名</th>
+                    <th>カテゴリ</th>
+                    <th>難易度</th>
+                    <th>予算（万円）</th>
+                    <th>導入手順</th>
+                </tr>
+            </thead>
+            <tbody>
+                <!-- ここに<tr><td>deadline</td><td>todo</td><tr>の形でデータが入る -->
+                <?= $output ?>
+            </tbody>
+        </table>
+    </fieldset>
+
+    <script>
+        // PHPのデータをJSに渡す
+        const resultArray = <?= json_encode($result) ?>;
+        // 配列からタグ生成し，#outputに表示する
+    </script>
+</body>
+
+</html>
